c860198303872e148ec3481c770bfba9409c9a11
hi-craft <363511503@qq.com>
2018-04-21 13:59:56 +0800

    new

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("footer ul li a").click(function() {
+    $("footer ul li a[name]").click(function() {
         var attr = $(this).attr("name");
 
         $("#components").load(attr + ".html");
