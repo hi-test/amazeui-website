926391d448125bbc88bfd7c626e582ef7f95883f
hi-craft <363511503@qq.com>
2018-05-03 19:05:04 +0800

    new

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 $(function() {
-    $("footer ul li a[name]").click(function() {
-        var attr = $(this).attr("name");
-
-        $("#components").load(attr + ".html");
-        return false;
-    });
+    $('.am-topbar-fixed-bottom').load('./components/footer.html');
+    $('header').load('./components/header.html');
+    $youziku.load("body", "77fd23900ba84be6ac87f8318703d8cc", "RS_XingKai");
+    $youziku.draw();
 });
