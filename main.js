f991efe9d3be0c29f166bf0fdd88ddcd2db67ba6
hi-craft <363511503@qq.com>
2018-05-03 21:16:29 +0800

    new

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ $(function() {
     $('header').load('./components/header.html');
     $youziku.load("body", "77fd23900ba84be6ac87f8318703d8cc", "RS_XingKai");
     $youziku.draw();
+    $('#demo18').swiper({
+        pagination: '#demo18 .swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true
+        }
+    });
 });
