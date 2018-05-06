14abd52dd6364cdf07677c568289652db9e2dc1a
hi-craft <363511503@qq.com>
2018-05-06 15:27:41 +0800

    new

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(function() {
     $('.am-topbar-fixed-bottom').load('./components/footer.html');
     $('header').load('./components/header.html');
-    $youziku.load("body", "77fd23900ba84be6ac87f8318703d8cc", "RS_XingKai");
+    // $youziku.load("body", "77fd23900ba84be6ac87f8318703d8cc", "RS_XingKai");
+    $youziku.load("body", "baec19cb5a604002854408f93e923754", "Source-Han-Light");
     $youziku.draw();
     $('#demo18').swiper({
         pagination: '#demo18 .swiper-pagination',
